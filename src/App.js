d5922357b18641c30ead8f930efff286104d768e
Lipon Roy <liponbdbd@gmail.com>
2023-03-12 20:23:26 +0600

    installed necessary packages and start the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div className=' h-screen w-full flex flex-col items-center justify-center'>
+				<span className=' text-4xl'>Project Start</span>
+				<span className=' text-small'>-Lipon Roy</span>
+			</div>
+		</>
+	);
+}
+
+export default App;
